6501969b2ea713d1cfee4b9575438a246ed4aa2c
AdeyBoy123 <groveraden@gmail.com>
2023-09-14 15:55:22 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a string you want to reverse. ");
+            string input = Console.ReadLine();
+
+            String reversed = new string(input.Reverse().ToArray());
+
+            Console.WriteLine("The original input reversed is: "+ reversed);
+        }
+    }
+}
